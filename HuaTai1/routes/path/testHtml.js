d860fdfebabb0e1879832492d63927189d665dab
sylar <>
2018-03-08 14:26:35 +0800

    int huatai

---
+++ HuaTai1/routes/path/testHtml.js
@@ -0,0 +1,13 @@
+
+var router = require('express').Router();
+var AV = require('leanengine');
+
+// 查询 Todo 列表
+router.get('/', function(req, res, next) {
+  res.render('path/testHtml', {
+      
+    });
+});
+
+
+module.exports = router;
