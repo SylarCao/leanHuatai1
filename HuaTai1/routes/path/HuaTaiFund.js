e811e62ef4d21f1d5535db2a6582ad840352b1f0
sylar <sylar@Sylar-Mac-Mini-2.local>
2018-04-24 14:48:44 +0800

    表格 画好了

--- HuaTai1/routes/path/HuaTaiFund.js
+++ HuaTai1/routes/path/HuaTaiFund.js
@@ -1,4 +1,5 @@
 'use strict';
+var moment = require('moment');
 var router = require('express').Router();
 var AV = require('leanengine');
 
@@ -9,6 +10,13 @@ router.get('/', function(req, res, next) {
   var query = new AV.Query(Todo);
   query.descending('createdAt');
   query.find().then(function(results) {
+  	for (var i=0; i<results.length; i++)
+		{
+			var aaa = results[i];
+			var time = aaa.get('time');
+			var tt = modifyTime(time);
+			aaa.set('time', tt);
+		}
     res.render('path/HuaTaiFund', {
       fund: results
     });
@@ -17,14 +25,13 @@ router.get('/', function(req, res, next) {
   }).catch(next);
 });
 
-// 新增 Todo 项目
-// router.post('/', function(req, res, next) {
-//   var content = req.body.content;
-//   var todo = new Todo();
-//   todo.set('content', content);
-//   todo.save().then(function(todo) {
-//     res.redirect('/todos');
-//   }).catch(next);
-// });
+function modifyTime(time){
+	var date = moment(time).date();
+	var month = moment(time).month() + 1;
+	var year = moment(time).year();
+	var rt = year + "-" + month + "-" + date
+	return rt;
+};
+
 
 module.exports = router;
